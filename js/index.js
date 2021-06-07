f6ee843ae1e82fb60b5eb021f7226a0857d29211
Francisco Carrillo Pérez <carrilloperezfrancisco@gmail.com>
2021-06-07 15:53:12 +0200

    :world: added Brugge

--- js/index.js
+++ js/index.js
@@ -65,7 +65,8 @@ var markers =[{"name": "Madrid","lat":40.4893538,"long": -3.6827461},
     {"name": "Spa", "lat": 50.492429, "long": 5.864695},
     {"name": "Gent", "lat": 51.0543422, "long": 3.7174243},
     {"name": "Leuven", "lat": 50.87959, "long": 4.70093},
-    {"name": "Luxembourg", "lat": 49.611622, "long": 6.131935}]
+    {"name": "Luxembourg", "lat": 49.611622, "long": 6.131935},
+    {"name": "Brugge", "lat": 51.20892, "long": 3.22424}]
 
 var map = L.map( 'map', {
   center: [51.505, -0.09],
