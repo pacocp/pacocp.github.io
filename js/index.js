7b3cb5b5bf8210f2b549f6fc5015c2aebe62a1c5
Francisco Carrillo Pérez <carrilloperezfrancisco@gmail.com>
2026-05-10 19:46:08 +0200

    Updated webpage

--- js/index.js
+++ js/index.js
@@ -133,9 +133,9 @@ var map = L.map("map", {
   zoom: 1,
 });
 
-L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    '&copy; <a href="https://www.openstreetmap.org/copyright" referrerpolicy="origin">OpenStreetMap</a>',
   subdomains: ["a", "b", "c"],
 }).addTo(map);
 
